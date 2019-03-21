06903cdb03465c6d59725e8c688efb0f3bff8e4d
Leah Hamilton <leahrhamilton@yahoo.com>
2019-03-21 10:48:55 -0400

    added index.js file with delete button working and add new item button working

---
+++ index.js
@@ -0,0 +1,49 @@
+$(function (){ 
+//on submit, add new item to shopping list
+    $('#js-shopping-list-form').submit( function(e) {
+        //create variable that grabs the value of the input form
+        const text = $('#shopping-list-entry').val();
+        //append to shopping list
+        $('.shopping-list').append
+            ('<li>' 
+                +'<span class="shopping-item">' 
+                    + text 
+                + '</span>' 
+                + '<button class="shopping-item-toggle">' 
+                    + '<span class="button-label">' 
+                        + 'check' 
+                    + '</span>' 
+                + '</button>' 
+                + ' '
+                + '<button class="shopping-item-delete">'
+                    + '<span class="button-label">' 
+                        + 'delete'
+                    + '</span>'
+                + '</button>'
+            +'</li>');
+        $('.shopping-list').val();
+        $('.shopping-list-entry').val();
+        //empty the form field
+        $('#shopping-list-entry').val('');
+        e.preventDefault();
+    });
+
+    //delete shopping list item on click, first target the delete button
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {                                                                                                                                                
+        //then remove the closest li element
+        $(this).closest('li').remove();
+    });
+/*
+    $('.shopping-list').on ('click', '.shopping-item-toggle', function() {   
+        $(this).find('.shopping-item').toggleClass('.shopping-item__checked'); 
+    });
+*/
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {   
+        $(this).closest('li').find('.shopping-item').toggleClass('.shopping-item__checked'); 
+    });
+
+})
+
+
+
+
